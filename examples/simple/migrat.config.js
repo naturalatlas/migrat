5706bafaaa79fcca807f9202faab464c39a263cc
Brian Reavis <brian@thirdroute.com>
2014-12-14 12:18:58 -0700

    Removed old folder.

--- examples/simple/migrat.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	localState: './migrations/.migratdb',
-	migrationsDir: './migrations'
-};
--- examples/simple/migrations/1414006573623-first.js
+++
@@ -1,3 +0,0 @@
-module.exports.up = function(context, callback) { callback(); };
-module.exports.down = function(context, callback) { callback(); };
-module.exports.check = function(context, callback) { callback(); };
